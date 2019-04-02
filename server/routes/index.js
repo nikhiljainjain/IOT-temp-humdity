4750c663b488d67ff23f85372fcdfc49347e7b9f
Nikhil Jain <nikhiljain.ashajain@gmail.com>
2019-04-02 17:23:57 +0000

    improvements

--- server/routes/index.js
+++ server/routes/index.js
@@ -26,17 +26,13 @@ function dataCollect(callback){
 
 
 router.get('/', function(req, res, next) {
-    const new_date = new Date();
-    const dateX = Date.now;//new_date.getDate();
-    //console.log(dateX);
-    Data.find({}/*{s_date : dateX}*/, "x y humdity",(err,read)=>{
+    Data.find({}, (err,read)=>{
         if (err) throw err;
         const something = read[read.length-1].humdity;
         read.forEach((val, index)=>{
             val._id = null;//delete val._id;
             val.humdity = null;
-        })
-        console.log(led);   
+        });
         res.render("first",{parameter : read, hum: something, led: led}); 
     });
 });
@@ -47,28 +43,23 @@ router.get("/test/",(req,res)=>{
 
 router.post("/data/rcv/nodemcu",(req,res)=>{
     let new_time = new Date();
-    //const new_min = new_time.getMinutes();
-    let new_hour = new_time.getHours() + (new_time.getMinutes()/60) + (5.30);
-    //let new_date = new_time.getDate();
-    //dataCollect((data)=>{    
-        let newData = {
-            y : req.body.temp,
-            humdity : req.body.humdity,
-            weather : "Mostly Sunny",//data.response.ob.weather,
-            x : new_hour,//.toFixed(5),
-            //s_date : new_date
-        };
-        if (req.body.temp != 'nan'){
-            Data.create(newData, (err, data)=>{
-                if (err) throw err;
-                const x_num = String(led);
-                res.send(x_num);
-            });
-        }else{
+    let x = new_time.getTime();
+    let newData = {
+        y : req.body.temp,
+        humdity : req.body.humdity,
+        x: x,
+    };
+    console.log(newData);
+    if (req.body.temp != 'nan'){
+        Data.create(newData, (err, data)=>{
+            if (err) throw err;
             const x_num = String(led);
             res.send(x_num);
-        } 
-        //});
+        });
+    }else{
+        const x_num = String(led);
+        res.send(x_num);
+    }
 });
 
 router.get("/led/green/",(req,res)=>{
